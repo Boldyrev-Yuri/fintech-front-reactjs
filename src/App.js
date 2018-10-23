c415d9dac68da8f525b32856c52a3e4f52f6dbc5
Boldyrev-Yuri <boldyrev_yuri@mail.ru>
2018-10-23 22:14:30 +0700

    Реализовал первую версию приложения основного домашнего задания

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import "./bootstrap.css";
 
-class App extends Component {
+class Counter extends Component {
+  
+  state = { counter: this.props.init };
+
+  increment = () => {
+    const pMessage = document.getElementById("message");
+    if (this.state.counter < this.props.max) {
+      if (this.state.counter + this.props.step >= this.props.max) {
+        pMessage.style.visibility = "visible";
+      } else {
+        pMessage.style.visibility = "hidden";
+      }
+      this.setState((state) => ({
+        counter: this.state.counter + this.props.step
+      }));
+    }
+  };
+
+  decrement = () => {
+    const pMessage = document.getElementById("message");
+    if (this.state.counter > -this.props.max) {
+      if (this.state.counter - this.props.step <= -this.props.max) {
+        pMessage.style.visibility = "visible";
+      } else {
+        pMessage.style.visibility = "hidden";
+      }
+      this.setState((state) => ({
+        counter: this.state.counter - this.props.step
+      }));
+    }
+  };
+
+  reset = () => {
+    const pMessage = document.getElementById("message");
+    pMessage.style.visibility = "hidden";
+    this.setState((state) => ({
+      counter: 0
+    }));
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div key="1" className="App">
+        <p key="11" className="lead">Значение счетчика: <span key="12">{this.state.counter}</span></p>
+        <button key="21" className="btn btn-success" onClick={this.increment}>Увеличить</button>
+        <button key="22" className="btn btn-danger" onClick={this.decrement}>Уменьшить</button>
+        <button key="23" className="btn btn-warning" onClick={this.reset}>Сбросить</button>
+        <p key="31" className="alert alert-info" id="message" style={{ textAlign: "center", visibility: "hidden"}}> {this.props.message} </p>
       </div>
     );
   }
 }
 
-export default App;
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Counter from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById("root");
+
+const page = <Counter init={0} step={1} max={4} message={"Счетчик достиг предельного значения"} />;
+
+ReactDOM.render(page, rootElement);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
